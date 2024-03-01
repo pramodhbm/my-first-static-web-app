b0410a956bc8a8c87a3a8b0ab363f9e149720be0
Pramodh Bettadapura <pramodhbm@outlook.com>
2024-03-01 17:33:24 +0530

    with an api

---
+++ api/function_app.py
@@ -0,0 +1,47 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.route(route="message")
+def message(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+@app.route(route="log", auth_level=func.AuthLevel.FUNCTION)
+
+def log(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This is log API.")
+    else:
+        return func.HttpResponse(
+             "This  is log API. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function App(props) {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
   const firstName = props.user.firstName;
   const lastName = props.user.lastName;
-  return <div>Hello {firstName} {lastName}</div>;
+  return <div>Hello {firstName} {lastName}. {text}</div>;
 }
 
 export default App;
